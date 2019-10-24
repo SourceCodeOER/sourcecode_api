2e5d5868caf40ff76fc6416a6eca681019729439
jy95 <jacques.yakoub@gmail.com>
2019-10-24 19:30:04 +0200

    Working on seeders

--- seeders/20191024000708-default-data.js
+++ seeders/20191024000708-default-data.js
@@ -12,11 +12,17 @@ module.exports = {
             isBetaMember: false
           }], {});
         */
+
+        let table_with_schema = (TableName) =>
+            (queryInterface.sequelize.options.hasOwnProperty("schema"))
+                ? {schema: queryInterface.sequelize.options.schema, tableName: TableName}
+                : TableName;
+
         return new Promise((resolve, reject) => {
             Promise.all([
                 // First create some Tag Categories
                 queryInterface
-                    .bulkInsert("Tag_Category", [
+                    .bulkInsert(table_with_schema("Tag_Categories"), [
                         {kind: "programming_language"},
                         {kind: "difficulty"},
                         {kind: "exercise_type"},
@@ -26,23 +32,21 @@ module.exports = {
                     ]),
                 // Second create some User
                 queryInterface
-                    .bulkInsert("User", [
-                        {username: "jy95", password: "jy95", fullName: "Jacques Y.", role: "admin"},
-                        {username: "jacques95", password: "jy95", fullName: "Jacques2", role: "user"},
+                    .bulkInsert(table_with_schema("Users"), [
+                        {email: "jy95@perdu.com", password: "jy95", fullName: "Jacques Y.", role: "admin"},
+                        {email: "jacques95@perdu.com", password: "jy95", fullName: "Jacques2", role: "user"},
                     ])
             ]).then((res) => {
-                    const users = res[0];
-                    const tags = res[1];
-                    console.log(tags);
+
 
                     // Second create some Tags
                     return queryInterface
-                        .bulkInsert("Tag", [
-                            {isValidated: true, text: "Java", category_id: 1},
-                            {isValidated: true, text: "C", category_id: 1},
-                            {isValidated: true, text: "easy", category_id: 2},
-                            {isValidated: true, text: "hard", category_id: 2},
-                            {isValidated: true, text: "fill_the_code", category_id: 3},
+                        .bulkInsert(table_with_schema("Tags"), [
+                            {isValidated: true, text: "Java", category_id: 1, createdAt: new Date(), updatedAt: new Date()},
+                            {isValidated: true, text: "C", category_id: 1, createdAt: new Date(), updatedAt: new Date()},
+                            {isValidated: true, text: "easy", category_id: 2, createdAt: new Date(), updatedAt: new Date()},
+                            {isValidated: true, text: "hard", category_id: 2, createdAt: new Date(), updatedAt: new Date()},
+                            {isValidated: true, text: "fill_the_code", category_id: 3, createdAt: new Date(), updatedAt: new Date()},
                         ])
                 }
             ).catch((error) => {
