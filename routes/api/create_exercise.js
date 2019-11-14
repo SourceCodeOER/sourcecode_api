8aee74706b3cc4dc1cb2418feff8d90aa3b75c4b
jy95 <jacques.yakoub@gmail.com>
2019-11-14 11:19:22 +0100

    Working on creating exercise

--- routes/api/create_exercise.js
+++ routes/api/create_exercise.js
@@ -1,6 +1,8 @@
 const models = require('../../models');
 const Promise = require("bluebird");
+
 const partition = require('lodash.partition');
+const groupBy = require('lodash.groupby');
 
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
@@ -17,13 +19,13 @@ const conditionBuilder = (array_data) => ({
             models.sequelize.where(
                 models.sequelize.fn("LOWER", models.sequelize.col("text")),
                 Op.eq,
-                models.sequelize.fn("LOWER", tag.text)
+                tag.text.toLowerCase()
             )
         ]
     }))
 });
 
-// Promise to retrieve
+// Promise to retrieve possible matches for new tag
 function find_tag_matches(new_tags) {
     return new Promise((resolve, reject) => {
         // no need to query DB if no new
@@ -43,6 +45,37 @@ function find_tag_matches(new_tags) {
     })
 }
 
+// Promise that try to match new_tags with the result in DB
+function matching_process(already_present_tags, new_tags, result_in_db) {
+    return new Promise(resolve => {
+        // set up structure for matching
+        console.log(already_present_tags);
+        let tags_lowercase_text = result_in_db.map(tag => {
+            tag.text = tag.text.toLowerCase();
+            return tag;
+        });
+        let tag_dictionary = groupBy(tags_lowercase_text, "text");
+        Object.keys(tag_dictionary).forEach(item => {
+            tag_dictionary[item] = groupBy(tag_dictionary[item], "category_id");
+        });
+        // do the matching process here
+        const [has_match, no_match] = partition(new_tags,
+            tag =>
+                tag.text.toLowerCase() in tag_dictionary
+                && tag.category_id in tag_dictionary[tag.text.toLowerCase()]
+        );
+        // takes the first match
+        resolve([
+            already_present_tags.concat(
+                has_match.map(tag => {
+                    return tag_dictionary[tag.text.toLowerCase()][tag.category_id][0].id
+                })
+            ),
+            no_match
+        ]);
+    });
+}
+
 module.exports = function (req, res, next) {
 
     // distinguish already present tags from new tags
@@ -51,12 +84,17 @@ module.exports = function (req, res, next) {
     // find tag matches for new_tags if already existing
     find_tag_matches(new_tags)
         .then(result => {
-            // do the matching process here
-            console.log(result);
-            next(new Error("NOT YET IMPLEMENTED"));
-        }).catch(err => {
+            // try to match them
+            return matching_process(already_present_tags, new_tags, result);
+        })
+        .then(
+            ([existent_tags, really_new_tags]) => {
+                console.log("YOLO");
+            }
+        )
+        .catch(err => {
             next(err);
-        });
+    });
 
 
     //next(new Error("NOT YET IMPLEMENTED"));
