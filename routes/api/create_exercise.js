e0ca0fb44a34fbd70fb98eb40b9f2430f5a905d9
jy95 <jacques.yakoub@gmail.com>
2019-11-14 09:32:16 +0100

    Refactor condition builder

--- routes/api/create_exercise.js
+++ routes/api/create_exercise.js
@@ -1,4 +1,4 @@
-const models  = require('../../models');
+const models = require('../../models');
 const Promise = require("bluebird");
 const partition = require('lodash.partition');
 
@@ -10,30 +10,32 @@ module.exports = function (req, res, next) {
     // distinguish already present tags from new tags
     const [already_present_tags, new_tags] = partition(req.body.tags, obj => Number.isInteger(obj));
     // where condition builder
-    const conditionBuilder = (array_data) => [
-        models.sequelize.where(
-            models.sequelize.fn("LOWER", models.sequelize.col("text")),
-            Op.eq,
-            models.sequelize.fn("ANY", array_data.map(tag => tag.text.toLowerCase()))
-        ),
-        models.sequelize.where(
-            models.sequelize.col("category_id"),
-            Op.eq,
-            models.sequelize.fn("ANY", array_data.map(tag => tag.category_id))
-        )
-    ];
+    const conditionBuilder = (array_data) => ({
+        [Op.or]:  array_data.map(tag => ({
+            [Op.and] : [
+                models.sequelize.where(
+                    models.sequelize.col("category_id"),
+                    Op.eq,
+                    tag.category_id
+                ),
+                models.sequelize.where(
+                    models.sequelize.fn("LOWER", models.sequelize.col("text")),
+                    Op.eq,
+                    models.sequelize.fn("LOWER", tag.text)
+                )
+            ]
+        }))
+    });
 
     new Promise((resolve, reject) => {
         // no need to query DB if no new
-        if ( new_tags.length === 0 ) {
+        if (new_tags.length === 0) {
             resolve([]);
         } else {
             // query database to find possible match before creating new tags
             return models.Tag.findAll({
                 attributes: ["id", "text", "category_id"],
-                where: {
-                    [Op.and]: conditionBuilder(new_tags)
-                }
+                where: conditionBuilder(new_tags)
             })
         }
     }).then(result => {
