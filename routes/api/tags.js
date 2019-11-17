5e774f278c401c770113e83346ccab0ba7e372f8
jy95 <jacques.yakoub@gmail.com>
2019-11-17 16:09:42 +0100

    GET /api/tags done

--- routes/api/tags.js
+++ routes/api/tags.js
@@ -1,9 +1,64 @@
 const express = require('express');
 const router = express.Router();
+const models = require('../../models');
+
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 router.get("/",(req, res, next) => {
-    // TODO
-    next(new Error("NOT YET IMPLEMENTED"));
+    const settings = {
+        tags_ids: req.query.tags_ids || [],
+        categories_ids: req.query.categories_ids || [],
+        state: req.query.state || "default"
+    };
+
+    let conditions = [];
+
+    // must we include conditions or not
+    if (settings.state !== "default") {
+        conditions.push({
+            isValidated: settings.state === "validated"
+        })
+    }
+    if (settings.tags_ids.length > 0) {
+        conditions.push({
+            id: {
+                [Op.in]: settings.tags_ids
+            }
+        })
+    }
+    if (settings.categories_ids.length > 0) {
+        conditions.push({
+            category_id: {
+                [Op.in]: settings.categories_ids
+            }
+        })
+    }
+
+    // create the findOptions
+    const options = {
+        attributes: [
+            ["id", "tag_id"],
+            ["text", "tag_text"],
+            "category_id",
+            "isValidated",
+            "version"
+        ],
+        // dynamic create the where clause
+        where: (conditions.length === 0)
+            ? {}
+            : (conditions.length === 1)
+                ? conditions[0]
+                : {
+                    [Op.and]: conditions
+                }
+    };
+
+    return models
+        .Tag
+        .findAll(options)
+        .then(result => res.json(result))
+        .catch(err => next(err));
 });
 
 router.put("/",(req, res, next) => {
