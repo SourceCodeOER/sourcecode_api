b3bc0bba14e9a9a8a8f2602de53074649c090a6b
jy95 <jacques.yakoub@gmail.com>
2019-11-17 16:52:58 +0100

    Finished endpoint /api/tags

--- routes/api/tags.js
+++ routes/api/tags.js
@@ -5,6 +5,7 @@ const models = require('../../models');
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
+// for fetching
 router.get("/",(req, res, next) => {
     const settings = {
         tags_ids: req.query.tags_ids || [],
@@ -61,14 +62,67 @@ router.get("/",(req, res, next) => {
         .catch(err => next(err));
 });
 
+// For update
+// TODO later secure that to prevent some mad genius to do stuff they can't
 router.put("/",(req, res, next) => {
-    // TODO
-    next(new Error("NOT YET IMPLEMENTED"));
+
+    //const updateDate = new Date();
+    const {
+        tag_id,
+        tag_text,
+        category_id,
+        isValidated,
+        version
+    } = req.body;
+
+    return models
+        .Tag
+        .update({
+            category_id: category_id,
+            text: tag_text,
+            isValidated: isValidated,
+            // sequelize doesn't seem to update that one
+            version: version + 1
+        }, {
+            where: {
+                [Op.and]: [{ id: tag_id }, { version : version}]
+            }
+        })
+        .then( (check) => {
+            const numberOfRowAffected = check[0];
+            // TODO find later which the optimisticLock error isn't throw
+            if (numberOfRowAffected !== 1) {
+                let error = new Error("It seems you are using an outdated version of this resource : Operation denied");
+                error.status = 409;
+                next(error)
+            } else {
+                res.status(200).end()
+            }
+        })
+        .catch(err => next(err));
 });
 
+// create a Tag Proposal
 router.post("/",(req, res, next) => {
-    // TODO
-    next(new Error("NOT YET IMPLEMENTED"));
+    const creationDate = new Date();
+    const {
+        text,
+        category_id
+    } = req.body;
+
+    return models
+        .Tag
+        .create({
+            text: text,
+            category_id: category_id,
+            // by default, consider a tag as not official
+            isValidated: false,
+            // some date
+            updateAt: creationDate,
+            createAt: creationDate
+        })
+        .then( () => res.status(200).end())
+        .catch( err => next(err));
 });
 
 module.exports = router;
