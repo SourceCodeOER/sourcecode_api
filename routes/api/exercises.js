79506ffd05130ea4d5a09c228d5f7a4a45388dc4
jy95 <jacques.yakoub@gmail.com>
2019-11-19 21:46:11 +0100

    Refactoring this spaghetti code in /api/exercises/{id}

--- routes/api/exercises.js
+++ routes/api/exercises.js
@@ -48,50 +48,11 @@ router.put("/:exerciseId", (req, res, next) => {
     // distinguish already present tags from new tags
     const [already_present_tags, new_tags] = partition(req.body.tags, obj => Number.isInteger(obj));
 
-    return Promise.all([
-        // Find current exercise version with its tags
-        models
-            .Exercise
-            .findAll({
-                where: {
-                    [Op.and]: [
-                        {id: id},
-                        {version: req.body.version}
-                    ]
-                },
-                include: [
-                    {
-                        model: models.Exercise_Tag,
-                        as: "tag_entries",
-                        attributes: [
-                            "tag_id"
-                        ]
-                    }
-                ],
-                rejectOnEmpty: true
-            }),
-        // Find possible match for new tag(s)
-        find_tag_matches(new_tags)
-            .then(result => matching_process(already_present_tags, new_tags, result))
-    ])
-        .then(([[exercise_with_tag_records], [new_tags, tags_to_be_inserted]]) => {
-            const old_tags = exercise_with_tag_records
-                .get("tag_entries")
-                .map(tag => tag.get("tag_id"));
-            // computes the changes in order to insert (or not) minimal number of new rows
-            // as we could add and remove tags, we must handle both cases at once
-            let changes = {
-                "added": difference(new_tags, old_tags),
-                "deleted": difference(old_tags, new_tags)
-            };
-            // delegate work to other promise
-            return Promise.resolve([changes, tags_to_be_inserted]);
-        })
+    return find_exercise_tags_and_search_possible_new_tags_match(
+        [id, req.body.version, new_tags, already_present_tags]
+    )
+        .then(result => compute_tag_changes(result))
         .then(([changes, tags_to_be_inserted]) => {
-            // if there is only additions in tags, we will use our hook to deal with the update part of precomputed data
-            // if not, we have to do that manually
-            const onlyAdditions = (changes["added"].length > 0 && changes["deleted"].length === 0);
-            const creationDate = new Date();
 
             // transaction here as if anything bad happens, we don't commit that to database
             return models
@@ -101,84 +62,10 @@ router.put("/:exerciseId", (req, res, next) => {
                 }, (t) => {
                     // if new tags to insert, do that
                     // if not, do nothing
-                    return ((tags_to_be_inserted.length === 0)
-                            ? Promise.resolve([])
-                            : models
-                                .Tag
-                                .bulkCreate(tags_to_be_inserted.map(tag => ({
-                                    // no matter of the kind of user, creating tags like that should be reviewed
-                                    isValidated: false,
-                                    text: tag.text,
-                                    category_id: tag.category_id,
-                                    // some timestamps must be inserted
-                                    updatedAt: creationDate,
-                                    createdAt: creationDate
-                                })), {
-                                    transaction: t,
-                                    // I must retrieve the inserted row(s) ids for later
-                                    returning: ["id"]
-                                })
-                    ).then((created_tags) => {
-                        // add the newly created tag(s) into "added" key
-                        // if empty, it does nothing ^^
-                        changes["added"] = changes["added"].concat(
-                            created_tags.map(tag => tag.id)
-                        );
-                        // multiple cases can occur here
-                        switch (true) {
-                            // 1. no changes in the tags : the PERFECT case
-                            case (changes["added"].length === 0 && changes["deleted"].length === 0):
-                                return Promise.resolve();
-
-                            // 2. Only additions in tags : The AVERAGE case
-                            // ( we can rely on the hook in Exercise_Tag to update precomputed data eg : "tags_ids" )
-                            case onlyAdditions:
-                                return models
-                                    .Exercise_Tag
-                                    .bulkCreate(changes["added"].map(tag => ({
-                                        tag_id: tag,
-                                        exercise_id: id
-                                    })), {
-                                        transaction: t
-                                    });
-
-                            // 3. additions and deletes : The HARD case
-                            // We have to manually do what the hook in Exercise_Tag does
-                            // as there is no "instances" with their API for bulkDestroy
-                            default:
-                                return Promise.all([
-                                    // insert the new tags
-                                    // we have to disable the hook on them for no recompute things two times
-                                    models
-                                        .Exercise_Tag
-                                        .bulkCreate(changes["added"].map(tag => ({
-                                            tag_id: tag,
-                                            exercise_id: id
-                                        })), {
-                                            transaction: t,
-                                            hooks: false
-                                        }),
-                                    // delete the old tags
-                                    models
-                                        .Exercise_Tag
-                                        .destroy({
-                                            where: {
-                                                [Op.and]: [
-                                                    {exercise_id: id},
-                                                    {
-                                                        tag_id: {
-                                                            [Op.in]: changes["deleted"]
-                                                        }
-                                                    }
-                                                ]
-                                            }
-                                        })
-                                ]).then(() => {
-                                    // retrieve the new "tags_ids" array & update the Exercise_metrics row
-                                    
-                                })
-                        }
-                    })
+                    return insert_new_tags_if_there_is_at_least_one(tags_to_be_inserted, t)
+                        .then((created_tags) =>
+                            handle_all_cases_for_tags([id, changes, created_tags, t])
+                        )
                 })
 
         })
@@ -202,3 +89,143 @@ router.put("/:exerciseId", (req, res, next) => {
 });
 
 module.exports = router;
+
+// some methods that made life easier with this hell of promise chaining
+function find_exercise_tags_and_search_possible_new_tags_match(
+    [
+        id,
+        version,
+        new_tags,
+        already_present_tags
+    ]
+) {
+    return Promise.all([
+        // Find current exercise version with its tags
+        models
+            .Exercise
+            .findAll({
+                where: {
+                    [Op.and]: [
+                        {id: id},
+                        {version: version}
+                    ]
+                },
+                include: [
+                    {
+                        model: models.Exercise_Tag,
+                        as: "tag_entries",
+                        attributes: [
+                            "tag_id"
+                        ]
+                    }
+                ],
+                rejectOnEmpty: true
+            }),
+        // Find possible match for new tag(s)
+        find_tag_matches(new_tags)
+            .then(result => matching_process(already_present_tags, new_tags, result))
+    ])
+}
+
+function compute_tag_changes([[exercise_with_tag_records], [new_tags, tags_to_be_inserted]]) {
+    const old_tags = exercise_with_tag_records
+        .get("tag_entries")
+        .map(tag => tag.get("tag_id"));
+    // computes the changes in order to insert (or not) minimal number of new rows
+    // as we could add and remove tags, we must handle both cases at once
+    let changes = {
+        "added": difference(new_tags, old_tags),
+        "deleted": difference(old_tags, new_tags)
+    };
+    // delegate work to other promise
+    return Promise.resolve([changes, tags_to_be_inserted]);
+}
+
+function insert_new_tags_if_there_is_at_least_one(tags_to_be_inserted, t) {
+    const creationDate = new Date();
+    if (tags_to_be_inserted.length === 0) {
+        return Promise.resolve([])
+    } else {
+        return models
+            .Tag
+            .bulkCreate(tags_to_be_inserted.map(tag => ({
+                // no matter of the kind of user, creating tags like that should be reviewed
+                isValidated: false,
+                text: tag.text,
+                category_id: tag.category_id,
+                // some timestamps must be inserted
+                updatedAt: creationDate,
+                createdAt: creationDate
+            })), {
+                transaction: t,
+                // I must retrieve the inserted row(s) ids for later
+                returning: ["id"]
+            })
+    }
+}
+
+function handle_all_cases_for_tags([id, changes, created_tags, t]) {
+    // if there is only additions in tags, we will use our hook to deal with the update part of precomputed data
+    // if not, we have to do that manually
+    const onlyAdditions = (changes["added"].length > 0 && changes["deleted"].length === 0);
+
+    // add the newly created tag(s) into "added" key
+    // if empty, it does nothing ^^
+    changes["added"] = changes["added"].concat(
+        created_tags.map(tag => tag.id)
+    );
+    // multiple cases can occur here
+    switch (true) {
+        // 1. no changes in the tags : the PERFECT case
+        case (changes["added"].length === 0 && changes["deleted"].length === 0):
+            return Promise.resolve();
+
+        // 2. Only additions in tags : The AVERAGE case
+        // ( we can rely on the hook in Exercise_Tag to update precomputed data eg : "tags_ids" )
+        case onlyAdditions:
+            return models
+                .Exercise_Tag
+                .bulkCreate(changes["added"].map(tag => ({
+                    tag_id: tag,
+                    exercise_id: id
+                })), {
+                    transaction: t
+                });
+
+        // 3. additions and deletes : The HARD case
+        // We have to manually do what the hook in Exercise_Tag does
+        // as there is no "instances" with their API for bulkDestroy
+        default:
+            return Promise.all([
+                // insert the new tags
+                // we have to disable the hook on them for no recompute things two times
+                models
+                    .Exercise_Tag
+                    .bulkCreate(changes["added"].map(tag => ({
+                        tag_id: tag,
+                        exercise_id: id
+                    })), {
+                        transaction: t,
+                        hooks: false
+                    }),
+                // delete the old tags
+                models
+                    .Exercise_Tag
+                    .destroy({
+                        where: {
+                            [Op.and]: [
+                                {exercise_id: id},
+                                {
+                                    tag_id: {
+                                        [Op.in]: changes["deleted"]
+                                    }
+                                }
+                            ]
+                        }
+                    })
+            ]).then(() => {
+                // retrieve the new "tags_ids" array & update the Exercise_metrics row
+
+            })
+    }
+}
