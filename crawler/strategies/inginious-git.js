6d93128ab40320f8fe9a59ae5f3f71535b85a915
jy95 <jacques.yakoub@gmail.com>
2019-11-21 18:46:04 +0100

    Working on crawler

--- crawler/strategies/inginious-git.js
+++ crawler/strategies/inginious-git.js
@@ -47,13 +47,57 @@ module.exports = async function (options) {
                         return {
                             "all_tags": extract_tags(doc, "tags"), // all the given tags
                             "course_name": doc.name,
-                            "file_location": course_data, // useful for later
+                            "course_path": course_data, // useful for later
                         };
                     }
                 )
             , "course_path");
 
+        // 2. Get an array of tags metadata
+        // As said before, V1 and V2 have a few differences :
+        // for a task, V2 only use the tags under the "categories" key of a task.yaml
+        // for V1
+
+        // Copied from https://github.com/UCL-INGI/INGInious/blob/master/inginious/common/tags.py
+        // as it is referred nowhere in INGINIOUS docs :
+        /*
+            The 'type' represents the behaviour of the tag:
+            - 0: Skill tags. It appear in blue. When the tag is activated it becomes green.
+                 We can perform tasks search on this tag.
+            - 1: Misconception tags. It does not appear until it is activated. It appear in red when activated.
+                 We can NOT perform tasks search on this tag.
+                 The tags are useful to highlight errors or misconceptions.
+            - 2: Category tags. Never appear. Only used for organisation and when we perform tasks search.
+        */
+        const exercises = exercises_data.map(exercise_data => {
+            // parse the given yaml file
+            const doc = yaml.safeLoad(fs.readFileSync(exercise_data, 'utf8'));
+            // if we can find the related course metadata , use that to get more info on this exercise
+            // Warning : as path , we must take the most specialized one ( aka the longest string
+            const course_match = Object
+                .keys(courses)
+                .filter(s => exercise_data.includes(s))
+                .reduce((a, b) => a.length > b.length ? a : b, '');
+            const course_data = (course_match.length > 0 ) ? courses[course_match] : {};
+            // To distinguish Tags from INGINIOUS, I will use this convention :
+            // If category is a integer, it means that it comes from Inginious
+            // If category is a string, it means it is one of our Tag Category
+            
+        });
+
+        return {
+            "exercises" : exercises,
+            // if the given platform has its own categories for tag, use them
+            "own_categories": {
+                0: "Skill",
+                1: "Misconception",
+                2: "Category"
+            }
+        }
+
+
     } catch (e) {
+        console.error(e);
         return {};
     }
 
@@ -67,8 +111,11 @@ function extract_tags(doc, rootLey) {
         return Object
             .entries(doc[rootLey])
             .reduce((acc, [key, value]) => {
-                acc[key] = {
-                    id: key,
+                // to handle both V1 and V2 case as tags can be found in course.yaml and task.yaml
+                // In LSINF1252, they used unvalidated tags in their task.yaml
+                const real_key = (value.hasOwnProperty("id")) ? value.id : key;
+                acc[real_key] = {
+                    id: real_key,
                     text: value.name,
                     category: value.type
                 };
