c02b9c461357eeae67204d0f686d4eed9e950f41
jy95 <jacques.yakoub@gmail.com>
2019-11-25 16:30:52 +0100

    First draft of bulk_create_or_find_tag_categories

--- routes/api/bulk_create_or_find_tag_categories.js
+++ routes/api/bulk_create_or_find_tag_categories.js
@@ -2,6 +2,43 @@ const models = require('../../models');
 const Promise = require("bluebird");
 const Sequelize = require("sequelize");
 
-module.exports = function (req, res, next) {
+const partition = require('lodash.partition');
 
+module.exports = function (req, res, next) {
+    Promise.all(
+        req.body.map(tag_category => {
+            return models
+                .Tag_Category
+                .findOrCreate({
+                    where: findOrCreateCategory(tag_category)
+                })
+        })
+    ).then(results => {
+        // As explained in docs, it return something like [instance, isCreated]
+        // https://sequelize.org/master/class/lib/model.js~Model.html#static-method-findOrCreate
+        // I just need the instance
+        res.json(results.map(result => result[0]));
+    }).catch(err => {
+        next(err);
+    });
 };
+
+// credits to https://webbjocke.com/javascript-check-data-types/#javascript-string
+const isString = (value) => typeof value === 'string' || value instanceof String;
+
+// Generate where conditions
+// TODO handle case with sub category LATER
+function findOrCreateCategory(tag_category) {
+    // default case
+    if (isString(tag_category)) {
+        return {
+            kind: tag_category
+        }
+    } else {
+        // Complex case
+        return {
+            kind: tag_category.text,
+            parent_category: tag_category.category
+        }
+    }
+}
