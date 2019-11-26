0bf3fa5e67533d4831014275b2ef944603fbbb80
jy95 <jacques.yakoub@gmail.com>
2019-11-26 16:53:20 +0100

    Working on Tests

--- tests/endpoints.test.js
+++ tests/endpoints.test.js
@@ -23,6 +23,12 @@ function getRandomInt(min, max) {
 // credits to https://stackoverflow.com/a/8511350/6149867
 const isObject = (obj) => typeof obj === 'object' && obj !== null;
 
+// credits to https://stackoverflow.com/a/58920031/6149867
+// Only way to prevent conflict
+function wait(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
 // Should be able to register and login
 // if not, we cannot test so much things ...
 beforeAll(async () => {
@@ -122,7 +128,7 @@ describe("Simple case testing", () => {
     });
 });
 
-describe("Complex scenario", () => {
+describe("Complex scenarios", () => {
     it("scenario n°1 : Creates a exercises / Find it / Update it 3 times", async () => {
         // retrieve some tag categories
         let response = await request
@@ -133,6 +139,7 @@ describe("Complex scenario", () => {
             .send(tag_categories)
             .expect(200);
         expect(response.body).toHaveLength(tag_categories.length);
+
         const tag_categories_ids = response.body.map(category => category.id);
         // create some tags
         response = await Promise.all(tags.map(tag => {
@@ -140,38 +147,124 @@ describe("Complex scenario", () => {
                 .post("/api/tags")
                 .set('Authorization', 'bearer ' + JWT_TOKEN)
                 .set('Content-Type', 'application/json')
-                .send({text: tag, category_id: tag_categories_ids[getRandomInt(0, tag_categories_ids.length -1)]})
+                .send({text: tag, category_id: tag_categories_ids[getRandomInt(0, tag_categories_ids.length - 1)]})
         }));
         expect(response).toHaveLength(tags.length);
+
         // take some tags
         response = await request
             .get("/api/tags")
             .set('Accept', 'application/json')
             .expect(200);
         const some_tags_ids = response.body.map(tag => tag.id).slice(0, tags.length);
+
         // creates one exercise
         const title = "HELLO WORLD";
-        const some_exercise_data = (title) => ({
+        const some_exercise_data = {
             "title": title,
             "description": "Some verrrrrrrrrry long description here",
             // try to use both existent tags and not
             tags: some_tags_ids.concat(
                 ["SOME_TAG1", "SOME_TAG2", "SOME_TAG3"].map(tag => ({
                     text: tag,
-                    category_id: tag_categories_ids[getRandomInt(0, tag_categories_ids.length-1)]
+                    category_id: tag_categories_ids[getRandomInt(0, tag_categories_ids.length - 1)]
                 }))
             )
-        });
-
-        response = await request
+        };
+        await request
             .post("/api/bulk_create_exercises")
             .set('Authorization', 'bearer ' + JWT_TOKEN)
             .set('Content-Type', 'application/json')
             .send([
-                some_exercise_data(title)
+                some_exercise_data
             ]).expect(200);
-        // research this exercise
-        console.log()
 
+        // research this exercise
+        response = await request
+            .post("/api/search")
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .send({
+                data: {
+                    title: title,
+                    tags: some_tags_ids
+                }
+            })
+            .expect(200);
+
+        expect(isObject(response.body)).toBeTruthy();
+        expect(Array.isArray(response.body.data)).toBeTruthy();
+        expect(response.body.data).toHaveLength(1);
+        expect(response.body.metadata.totalItems).toBe(1);
+        let wrap_exercise_data = ([{title, description, id, version, tags}]) => ({
+            title,
+            description,
+            id,
+            version,
+            tags
+        });
+        let data = wrap_exercise_data(response.body.data);
+        expect(data.version).toBe(0);
+
+        // test all updates cases : add / remove / keep tags
+        // 1. Only changed description
+
+        response = await request
+            .put("/api/exercises/" + data.id)
+            .set('Authorization', 'bearer ' + JWT_TOKEN)
+            .set('Content-Type', 'application/json')
+            .send({
+                title: data.title,
+                version: data.version,
+                description: data.description + "API4FUN",
+                tags: data.tags.map(tag => tag.tag_id)
+            });
+
+        expect(response.status).toBe(200);
+
+        response = await request
+            .get("/api/exercises/" + data.id)
+            .set('Accept', 'application/json');
+
+        expect(response.status).toBe(200);
+
+        expect(isObject(response.body)).toBeTruthy();
+        expect(response.body.version).toBe(1);
+        expect(response.body.id).toBe(data.id);
+        /*
+        // 2. Only additions
+        await request
+            .put("/api/exercises/" + data.id)
+            .set('Authorization', 'bearer ' + JWT_TOKEN)
+            .set('Content-Type', 'application/json')
+            .send({
+                title: response.body.title,
+                version: response.body.version + 1,
+                description: response.body.description,
+                tags: response.body.tags.map(tag => tag.tag_id).concat([
+                    {text: "TRY 42", category_id: 1}
+                ])
+            })
+            .expect(200);
+
+         */
+        // 3. Add / remove some tags ( difficult case )
+        /*
+        await request
+            .put("/api/exercises/" + data.id)
+            .set('Authorization', 'bearer ' + JWT_TOKEN)
+            .set('Content-Type', 'application/json')
+            .send({
+                title: data.title,
+                version: data.version + 1,
+                description: data.description + "API4FUN",
+                tags: data.tags.splice(0, 1).map(tag => tag.tag_id).concat([
+                    {text: "TRY 1", category_id: tag_categories_ids[getRandomInt(0, tag_categories_ids.length - 1)]},
+                    {text: "TRY 2", category_id: tag_categories_ids[getRandomInt(0, tag_categories_ids.length - 1)]},
+                ])
+            })
+            .expect(200);
+
+         */
     });
 });
