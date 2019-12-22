6d1f4ed3061d704701e5e8bf175b542fee42b62e
jy95 <jacques.yakoub@gmail.com>
2019-12-22 17:00:22 +0100

    Use my fork while my PR is reviewed

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const helmet = require('helmet');
 
 // OpenAPI V3 validation middleware
 const Enforcer = require("openapi-enforcer-middleware");
-const enforcerMulter = require('openapi-enforcer-multer');
+// TEMPORARY use my fork to wait that my PR is merged
+const enforcerMulter = require('@jy95/openapi-enforcer-multer');
 const spec = path.join(__dirname, 'api.yml');
 const controllerDirectory = path.resolve(__dirname, 'controllers');
 
