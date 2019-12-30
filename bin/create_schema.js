0824aca173d86eeb1406ceaac045d33e640a0b18
Jacques Yakoub <jacques.yakoub@gmail.com>
2019-12-30 18:04:44 +0100

    Improvements (#31)
    * Add security for ChangeExercisesStatus / DeleteExercises
    
    * doc(api) : Add documentation on static endpoints
    
    * Simplify setup instructions
    
    * PUT:  /api/exercises/{id} - add removePreviousFile
    
    Feature requested on #29 implemented

---
+++ bin/create_schema.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV /* istanbul ignore next */ || 'development';
+const debug = require(__dirname + '/../controllers/_common/debug');
+const config = require(__dirname + '/../config/config.json')[env];
+const createSchema = `
+    DROP SCHEMA IF EXISTS ${config.schema} CASCADE;
+    CREATE SCHEMA ${config.schema};
+`;
+
+try {
+    let sequelize;
+    /* istanbul ignore next */
+    if (config.use_env_variable) {
+        sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    } else {
+        sequelize = new Sequelize(config.database, config.username, config.password, config);
+    }
+
+    sequelize
+        .query(createSchema, {
+            type: sequelize.QueryTypes.RAW
+        })
+        .then( () => {
+            process.exit(0);
+        })
+        .catch( (err) => {
+            debug.errors("%O", err);
+            throw err;
+        });
+
+} catch (e) {
+    process.exit(1);
+}
