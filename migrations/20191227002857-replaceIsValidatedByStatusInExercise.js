a1befb8b486da11dc6f2bd4bdc019d06eb9f6500
jy95 <jacques.yakoub@gmail.com>
2020-03-01 18:51:22 +0100

    chore(migration) : Make migrations great again
    I had enough to deal with strange behaviors on @dewita laptop ^^

--- migrations/20191227002857-replaceIsValidatedByStatusInExercise.js
+++ migrations/20191227002857-replaceIsValidatedByStatusInExercise.js
@@ -7,13 +7,17 @@ module.exports = {
         if (queryInterface.sequelize.options.schema) {
             opts.schema = queryInterface.sequelize.options.schema;
         }
-        return Promise.all([
-            queryInterface.removeColumn(opts, "isValidated"),
-            queryInterface.addColumn(opts, "state", {
+
+        return queryInterface.sequelize.transaction(async (t) => {
+
+            await queryInterface.removeColumn(opts, "isValidated", {transaction: t});
+            await queryInterface.addColumn(opts, "state", {
                 type: Sequelize.ENUM("DRAFT", "PENDING", "VALIDATED", "NOT_VALIDATED"),
                 defaultValue: "DRAFT"
-            })
-        ]);
+            }, {transaction: t});
+
+            return Promise.resolve();
+        });
     },
 
     down: (queryInterface, Sequelize) => {
