bd71efadc230bcd0cc1f212cd702ed73fc103633
jy95 <jacques.yakoub@gmail.com>
2020-03-07 19:28:47 +0100

    feat(controller) : Add tags total - WIP

--- controllers/tags_categories/getTagCategoriesWithTags.js
+++ controllers/tags_categories/getTagCategoriesWithTags.js
@@ -1,7 +1,18 @@
 const models = require('../../models');
-const Sequelize = require("sequelize");
-const Op = Sequelize.Op;
-const {TAGS: TagState} = require("../_common/constants");
+const {Op, literal: SequelizeLitteral} = require("sequelize");
+const {TAGS: TagState, EXERCISES} = require("../_common/constants");
+// to map request exercise state(s) to column in database, as Sequelize cannot use views yet :(
+// !! Should be rewritten one day !!
+const mapToDb = {
+    [EXERCISES.PENDING]: "total_pending",
+    [EXERCISES.NOT_VALIDATED]: "total_not_validated",
+    [EXERCISES.VALIDATED]: "total_validated",
+    [EXERCISES.ARCHIVED]: "total_archived",
+    [EXERCISES.DRAFT]: "total_draft",
+};
+const schema = models.sequelize.options.schema;
+/* istanbul ignore next */
+const WithSchema = (schema, table) => (schema) ? `"${schema}"."${table}"` : `"${table}"`;
 
 module.exports = function (req, res, next) {
     const params = req.query;
@@ -9,6 +20,7 @@ module.exports = function (req, res, next) {
     const settings = {
         state: params.state || [],
         onlySelected: params.onlySelected /* istanbul ignore next */ || [],
+        countStates: params.countStates || Object.values(EXERCISES),
     };
 
     let criteria = [];
@@ -39,7 +51,16 @@ module.exports = function (req, res, next) {
                     ["text", "tag_text"],
                     "category_id",
                     "state",
-                    "version"
+                    "version",
+                    // Honestly it is quite of ugly but no choice : as explained, sequelize don't support views yet
+                    [
+                        SequelizeLitteral(`(
+                            SELECT COALESCE(SUM(${settings.countStates.map(s => "stats." + mapToDb[s]).join(" + ")}) ,0)
+                            FROM ${WithSchema(schema, "tags_by_exercise_state")} AS stats
+                            WHERE stats.tag_id = tags.id
+                        )`),
+                        "total"
+                    ]
                 ],
                 as: "tags",
                 where: Object
@@ -49,7 +70,7 @@ module.exports = function (req, res, next) {
                                 /* istanbul ignore next */
                                 ? [{
                                     state: {
-                                        [Op.in]: settings.state.map(s => TagState[s] )
+                                        [Op.in]: settings.state.map(s => TagState[s])
                                     }
                                 }]
                                 : []
@@ -58,16 +79,25 @@ module.exports = function (req, res, next) {
             }
         ],
         order: [
-            ["id", "ASC"]
+            ["id", "ASC"],
+            //["tags.total", "DESC"]
         ]
     };
 
     return models
         .Tag_Category
         .findAll(options)
-        .then(result => res.send(
-            result.map(/* istanbul ignore next */cat => cat.toJSON()))
-        )
+        .then(result => {
+            let categories = result.map(cat => {
+                let newCat = cat.toJSON();
+                newCat["tags"] = newCat["tags"].map(tag => {
+                    tag["total"] = Number(tag["total"]);
+                    return tag;
+                });
+                return newCat;
+            });
+            res.send(categories);
+        })
         .catch(/* istanbul ignore next */
             err => next(err));
 };
